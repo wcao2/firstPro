8718b634cb2f175db2f9d3c4008e164238bae6e0
wcao2 <caowei20171218@gmail.com>
2020-04-30 21:51:54 -0400

    based on diff profile to produce diff s3Client

--- src/test/java/com/ascendingdc/training/config/AWSConfigTest.java
+++ src/test/java/com/ascendingdc/training/config/AWSConfigTest.java
@@ -1,6 +1,10 @@
 package com.ascendingdc.training.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,7 +15,26 @@ import static org.mockito.Mockito.*;
 @Profile("unit")
 public class AWSConfigTest {
     @Bean
+    //when do unit test, Inject a hypothetical bean
     public AmazonS3 getAmazonS3(){
         return mock(AmazonS3.class);
     }
+
+    @Bean
+    public AmazonSQS getAmazonSQS(){
+        return AmazonSQSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain()).withRegion(Regions.US_EAST_1).build();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
